95930a61fbf34421e26ce79072eae0a2e0234878
Jalia Evans <irohdesign@outlook.com>
2018-05-02 21:51:33 -0400

    finished smooth scroll effect

--- main.js
+++ main.js
@@ -5,7 +5,17 @@ $(document).on('click','.navbar-collapse.in',function(e) {
 });
 
 $(document).ready(function() {
-	$('.scroll').on('click', function(event) {
-		
-	})
-})
+	$("a").on('click', function(event) {
+		if(this.hash !== "") {
+			event.preventDefault();
+			
+			var hash = this.hash;
+			
+			$('html,body').animate({
+				scrollTop: $(hash).offset().top
+			}, 800, function() {
+				window.location.hash = hash;
+			});
+		}
+	});
+});
