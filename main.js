cc7f432a396d138c8e7ca910056fdc1c66a8d3e8
Jalia Evans <irohdesign@outlook.com>
2018-05-02 21:25:23 -0400

    added scroll animation using jQuery animate function

--- main.js
+++ main.js
@@ -8,6 +8,12 @@ $(document).ready(function() {
 	
 	var scrollLink = $('.scroll');
 	
+	scrollLink.click(function(e) {
+		e.preventDefault();
+		
+		$('body,html').animate({
+			scrollTop:$(this.hash).offset().top
+		} )
+	})
 	
-	
-});
+})
